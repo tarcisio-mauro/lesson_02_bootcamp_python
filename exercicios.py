07c0e85c68687fe3bdab6ee6d7b3e20a2073b53c
tarcisio.mauro <tarcisio.mauro@cloudpay.net>
2025-07-22 10:41:06 +0200

    resolvendo alguns exercícios sozinho

--- exercicios.py
+++ exercicios.py
@@ -3,19 +3,37 @@ import math
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+# numero_1 = int(input("inserir um número inteiro: "))
+# numero_2 = int(input("inserir outro número inteiro: "))
+# resultado = numero_1 + numero_2
+# print(f"O resultado da soma é {resultado}")
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# numero_usuario = float(input("Insira um número: "))
+# CTE_DIVISAO = 5
+# resultado = numero_usuario % CTE_DIVISAO
+# print(f"O resto da divisão por {CTE_DIVISAO} do número inserido é {resultado}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+numero_1 = int(input("Insira  um número inteiro: "))
+numero_2 = int(input("Insira outro número inteiro: "))
+resultado_multiplicacao = numero_1 * numero_2
+print(f"O resultado da multiplicação entre os números é {resultado_multiplicacao}")
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
-try:
-    numero_1 = int(input("inserir um número inteiro: "))
-    numero_2 = int(input("inserir outro número inteiro: "))
-    resultado = numero_1 // numero_2
-    print(resultado)
-except ZeroDivisionError:
-    print("integer division or module by zero")
-except KeyboardInterrupt:
-    print("Acho que você não quis inserir um número")
+# try:
+#     numero_1 = int(input("inserir um número inteiro: "))
+#     numero_2 = int(input("inserir outro número inteiro: "))
+#     resultado = numero_1 // numero_2
+#     print(resultado)
+# except ZeroDivisionError:
+#     print("integer division or module by zero")
+# except KeyboardInterrupt:
+#     print("Acho que você não quis inserir um número")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
