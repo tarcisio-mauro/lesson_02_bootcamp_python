affe9306cb41ae19005e6ad70079a3e2872f1653
tarcisio.mauro <tarcisio.mauro@cloudpay.net>
2025-08-01 16:15:06 +0200

    very short study session

--- exercicios.py
+++ exercicios.py
@@ -117,6 +117,13 @@ import statistics
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+# need to verify this
+# value1 = bool(input("Type either True or False for value 1: "))
+# value2 = bool(input("Type either True or False for value 2: "))
+# and_result = value1 and value2
+# print(f"The result of value 1 and value 2 is {and_result}")
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
