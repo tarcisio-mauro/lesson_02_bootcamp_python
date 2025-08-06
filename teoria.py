bd505f64bbb91f55b7b88a11e0ef65dd5c8fe47f
tarcisio.mauro <tarcisio.mauro@cloudpay.net>
2025-08-06 13:18:54 +0200

    adding a separate python study file to learn about try-except

---
+++ teoria.py
@@ -0,0 +1,10 @@
+# TypeError, Type Check e Type Conversion em Python
+
+# Exemplo que causa TypeError
+
+resultado = len(5)
+
+# try:
+#     resultado = len(5)
+# except TypeError as e:
+#     print(e)  # Imprime a mensagem de erro
